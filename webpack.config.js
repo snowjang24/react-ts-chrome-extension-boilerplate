804f1abb6acd38345a912ef40793f3bd70c7d685
snowjang24 <asdka4618@gmail.com>
2020-06-05 08:37:24 +0900

    feat: init with crx and babel

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const port = process.env.PORT || 8000;
+module.exports = {
+  mode: "development",
+};
