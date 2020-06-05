9ce4f5fe6b8eef82dce54edd666e16e97da77b9c
snowjang24 <asdka4618@gmail.com>
2020-06-06 00:41:59 +0900

    feat: add locale of chrome extension

--- build-utils/tasks.js
+++ build-utils/tasks.js
@@ -4,7 +4,7 @@ exports.copyAssets = () => {
   rm("-rf", "dist");
   mkdir("dist");
   cp(`chrome/manifest.json`, `dist/manifest.json`);
-  // cp("-R", "chrome/assets/*", "dist");
+  cp("-R", "chrome/assets/*", "dist");
   cp("-R", "chrome/views/*", "dist");
   mkdir(`dist/js`);
 };
